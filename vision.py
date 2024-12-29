8511f7270b34dfc7e49a9782e0473735201433e4
Sarita <126271555+SaritaPhD@users.noreply.github.com>
2024-12-30 02:12:10 +0530

    Update vision.py

--- vision.py
+++ vision.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env.
 
 import streamlit as st 
-import os
+import os 
 import pathlib
 import textwrap 
 from PIL import Image
