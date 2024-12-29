ba1751b95f7fe005ff9f0392318265eb5f751f20
Sarita <126271555+SaritaPhD@users.noreply.github.com>
2024-12-30 02:12:39 +0530

    Update vision.py

--- vision.py
+++ vision.py
@@ -6,7 +6,7 @@ load_dotenv()  # take environment variables from .env.
 import streamlit as st  
 import os 
 import pathlib
-import textwrap 
+import textwrap  
 from PIL import Image
 
 import google.generativeai as genai
