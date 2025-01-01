98c09328a570463eb90c7b2ff6b96b18695806d4
Sarita <126271555+SaritaPhD@users.noreply.github.com>
2025-01-01 20:12:20 +0530

    Update vision.py

--- vision.py
+++ vision.py
@@ -14,7 +14,7 @@ import google.generativeai as genai
 
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
+ 
 ## Function to load OpenAI model and get respones
 
 def get_gemini_response(input,image,prompt):
